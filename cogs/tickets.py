2e563cea7d1c8c4e0cd6fe99a88fd713b164e10d
olxver <123242807+olxver@users.noreply.github.com>
2023-04-15 16:50:38 +0100

    Update tickets.py

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -12,3 +12,30 @@ from nextcord.ext import application_checks
 
 
 
+class Tickets(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @discord.slash_command(description="Parent command for tickets")
+    @commands.has_guild_permissions(manage_messages=True)
+    @commands.cooldown(1, 10, BucketType.guild)
+    async def tickets(self, interaction:Interaction):
+        pass
+
+    @tickets.subcommand(description="Create a message in which a user can create a ticket, with buttons")
+    @commands.has_guild_permissions(manage_messages=True)
+    @commands.cooldown(1, 10, BucketType.guild)
+    async def create(self, interaction:Interaction, 
+                    name:str=SlashOption(description="The name to display for the button?", required=True),
+                    ticket_name=SlashOption(description="Do you want the name to count up based on the amount of tickets created or based on the users name?", choices={"count up", "user's name"}, required=True),
+                    desc:str=SlashOption(description="The description for the embed? (add imgur source links if you want e.g i.imgur.com/example.png)", required=False)):
+
+        
+        await interaction.send(f"{name},{desc},{ticket_name}")
+
+
+
+
+def setup(bot):
+    bot.add_cog(Tickets(bot))
+    print("Tickets cog loaded")
